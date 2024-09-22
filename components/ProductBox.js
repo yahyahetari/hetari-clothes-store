fb5f0e394d1e27c8286548190637fca65f9d6a60
yahyahetari <132639815+yahyahetari@users.noreply.github.com>
2024-09-22 20:27:18 +0300

    up

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -166,7 +166,7 @@ export default function ProductBox({ _id, title, description, images, price, cat
               <img
                 src={images[0]}
                 alt="product"
-                className="w-[130px] h-[170px] sm:w-[170px] sm:h-[220px] md:w-[180px] md:h-[260px] lg:w-[180px] lg:h-[260px] xl:w-[200px] xl:h-[280px] rounded-md m-1.5 transition-transform duration-300 group-hover:scale-105 bg-white object-cover cursor-pointer"
+                className="w-[135px] h-[170px] sm:w-[170px] sm:h-[220px] md:w-[180px] md:h-[260px] lg:w-[180px] lg:h-[260px] xl:w-[200px] xl:h-[280px] rounded-md m-1.5 transition-transform duration-300 group-hover:scale-105 bg-white object-cover cursor-pointer"
                 onLoad={() => setIsImageLoaded(true)}
               />
               <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center opacity-0 group-hover:opacity-95 transition-opacity duration-300">
@@ -183,7 +183,7 @@ export default function ProductBox({ _id, title, description, images, price, cat
                 <span key={index}>{value}</span>
               ))}
               {additionalColorsCount > 0 && (
-                <span className="text-sm text-gray-800  absolute -top-1 right-12 sm:right-12 md:right-20 xl:right-24 ">
+                <span className="text-sm text-gray-800  absolute -top-1 right-7 sm:right-12 md:right-20 xl:right-24 ">
                   +{additionalColorsCount}
                 </span>
               )}
