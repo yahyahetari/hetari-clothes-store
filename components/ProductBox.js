5381c909a45203953726cd4fadfff73e54160be2
yahyahetari <132639815+yahyahetari@users.noreply.github.com>
2024-09-23 21:15:51 +0300

    up

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -151,16 +151,16 @@ export default function ProductBox({ _id, title, description, images, price, cat
                 <img
                   src={images[0]}
                   alt="product"
-                  className="w-[130px] h-[170px] sm:w-[170px] sm:h-[220px] md:w-[180px] md:h-[260px] lg:w-[180px] lg:h-[260px] xl:w-[200px] xl:h-[280px] rounded-md m-1.5 transition-transform duration-300 group-hover:scale-105 bg-white object-cover cursor-pointer"
+                  className="w-[130px] h-[160px] sm:w-[170px] sm:h-[220px] md:w-[180px] md:h-[260px] lg:w-[170px] lg:h-[240px] xl:w-[180px] xl:h-[260px] rounded-md m-1.5 transition-transform duration-300 group-hover:scale-105 bg-white object-cover cursor-pointer"
                   onLoad={() => setIsImageLoaded(true)}
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center opacity-0 group-hover:opacity-95 transition-opacity duration-300">
+                <div className="absolute inset-0 rounded-t-md bg-black bg-opacity-40 flex items-center justify-center opacity-0 group-hover:opacity-95 transition-opacity duration-300">
                   <span className="text-white text-lg font-semibold">View Details</span>
                 </div>
               </div>
             </Link>
             <div className="pl-2 pb-1 text-left flex-grow flex flex-col">
-              <p className="text-md font-semibold" title={title}>
+              <p className="text-sm sm:text-base font-semibold" title={title}>
                 {truncateText(title, 17)}
               </p>
               <div className="text-sm flex flex-wrap -mb-2.5 gap-0.5 flex-grow relative">
@@ -168,7 +168,7 @@ export default function ProductBox({ _id, title, description, images, price, cat
                   <span key={index}>{value}</span>
                 ))}
                 {additionalColorsCount > 0 && (
-                  <span className="text-sm text-gray-800  absolute -top-1 right-7 sm:right-12 md:right-20 xl:right-24 ">
+                  <span className="text-sm text-gray-800  absolute -top-1 right-7 sm:right-16 md:right-20 lg:right-16 xl:right-20 ">
                     +{additionalColorsCount}
                   </span>
                 )}
